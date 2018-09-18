f49e8a76aeb32d2328600bdac47034d065fd40c3
tleytek <lucidmat94@gmail.com>
2018-09-17 21:39:04 -0400

    mysql burger seeds

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Cheese Burger', '0');
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Big Mac', '0');
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Chicken Burger', '0');
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Whopper', '0');
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Double Hamburger', '0');
