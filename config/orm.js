6a57b6e990d8aab84a2dd5c09c3ac91971dff24b
tleytek <lucidmat94@gmail.com>
2018-09-19 16:56:59 -0400

    orm's added

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,34 @@
 var connection = require("./connection.js");
 
 var orm = {
-  selectAll: function(tableInput) {
-    var queryString = "SELECT * FROM ??";
-    connection.query(queryString, [tableInput], function(err, result) {
+  selectAll: function(callback) {
+    var queryString = "SELECT * FROM burgers";
+    connection.query(queryString, function(err, result) {
       if (err) throw err;
-      console.log(result);
+      callback(result);
+    });
+  },
+
+  insertOne: function(burger_name, callback) {
+    var queryString =
+      "INSERT INTO burgers (burger_name, devoured) VALUES (??, '0');";
+    connection.query(queryString, [burger_name], function(err, result) {
+      if (err) throw err;
+      callback(result);
+    });
+  },
+
+  updateOne: function(burger_name, devoured, callback) {
+    var queryString =
+      "UPDATE burgers SET devoured = ?? WHERE burger_name = ??;";
+    connection.query(queryString, [burger_name, devoured], function(
+      err,
+      result
+    ) {
+      if (err) throw err;
+      callback(result);
     });
   }
 };
+
+module.exports = orm;
