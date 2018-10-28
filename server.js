ffce468126c37c4297a3fac8f30fb2aa35d52aa4
tleytek <lucidmat94@gmail.com>
2018-10-27 20:30:02 -0400

    added background, still looks like crap, I have a bad eye for looks

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+//Stating the root folder for all of our assets
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,6 +24,6 @@ app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+	// Log (server-side) when our server has started
+	console.log("Server listening on: http://localhost:" + PORT);
 });
